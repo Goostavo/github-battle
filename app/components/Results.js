4f88b90b9b988d0ffe4a09a646f4de63c366d22e
Gustavo Garcia <goostavobg@gmail.com>
2017-07-23 17:59:36 -0300

    Implemented the Battle Results funcionality using "json mode"

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,66 @@
 var React = require('react');
+const QueryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
 
 class Results extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+  componentDidMount() {
+    let players = QueryString.parse(this.props.location.search);
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName]
+    ).then((results) => {
+      if (results === null){
+        return this.setState(() => {
+          return {
+            error: 'Looks like there was an error!\nCheck that both users exist on github.',
+            loading: false
+          }
+        });
+      }
+
+      return this.setState(() => {
+        return {
+          error: null,
+          loading: false,
+          winner: results[0],
+          loser: results[1]
+
+        }
+      });
+    });
+  }
   render() {
+    let error = this.state.error;
+    let winner = this.state.winner;
+    let loser = this.state.loser;
+    let loading = this.state.loading;
+
+    if (loading === true){
+      return (<p>Loading...</p>)
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <div>Results</div>
+      <div>{JSON.stringify(this.state, null, 2)}</div>
     )
   }
 }
